ca1ddd61bc81d644cc77a3a766234427e1a2d9a5
kekehua <kenneth443641@gmail.com>
2022-04-11 15:45:22 -0500

    Changes made to improve clarity

--- Develop/script.js
+++ Develop/script.js
@@ -1,52 +1,79 @@
 // Assignment Code
+// assigns the id generate to the generateBtn variable
 var generateBtn = document.querySelector("#generate");
 
 function writePassword() {
+// password length variable
 let pwLength;
+
+// Special Characters variable
 let pwSpecial;
+
+// Capital Letters variable
 let pwCapital;
+
+// Numbers variable
 let pwNumber;
+
+// Password Generation Function
 function generatePassword () {
+		// Sets strings of certain password criterion into constants
 		const alpha = "abcdefghijklmnopqrstuvwxyz";
 		const Capital = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 		const numbers = "1234567890";
 		const special = "!@#$%^&*_-+=";
+		// Starts the characters variable with the base set of lowercase letters
 		let characters = alpha;
+		// concatenates certain characters depending on user input
 		if (pwSpecial == true) {characters += special};
 		if (pwCapital == true) {characters += Capital};
 		if (pwNumber == true) {characters += numbers};
-			let password = "";
+		// Generates Password with criterion chosen	
+		let password = "";
 			for (let i = 0; i < pwLength; i++) {
 			password += characters.charAt(
 				(Math.floor(Math.random()*characters.length))
 			);
 		};
+		// Prints password to the page
 		var passwordText = document.querySelector("#password");
 		passwordText.value = password;
 	};	
-pwLength = window.prompt("What length would you like your password? (minimum 8 characters)", "Integer 8 or greater");
 
+	// Password Criterion Prompts
+	// Creates a pop-up that prompts the user how long they want their password to be. Must be between 8-128 character.
+	pwLength = window.prompt("What length would you like your password? (minimum 8 characters)", "Integer 8 or greater");
+		// Rejects answers above 128
 		if (pwLength > 128) {
 			window.alert("Please enter a value between 8 and 128!"); 
 			return;
 			}
+
+		// Proceeds to prompt users if they want special characters in their password
 		if (pwLength >= 8) {
 			pwSpecial = window.confirm("Would you like special characters? (Confirm:Yes, Cancel:No)");
 		}
+
+		// Rejects answers under 8
 		else {window.alert("Please enter a value betweem 8 and 128!"); 
 			return;
 		};
 		
+		// Prompts users if they want capital letters in their password
 		pwCapital = window.confirm("Would you like CAPITAL LETTERS? (Confirm:Yes, Cancel:No)");
 		
+		// Prompts users if they want numbers in their password
 		pwNumber = window.confirm("Would you like numbers? (Confirm:Yes, Cancel:No)");
 		
+		// Rejects responses that say no to having no special characters,capital, and numbers in their password
 		if ((pwSpecial == false)&&(pwCapital == false)&&(pwNumber == false)) {
 			window.alert("Please say yes to one of the prompts to have a strong password!");
 			return;
 		};
+
+		// Calls generatePassword function after requisite responses are filled
 		generatePassword();
 	};		
-// Add event listener to generate button
+// Event listener that calls writePassword function on event of click
 generateBtn.addEventListener("click", writePassword)
   
--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # Password-Generator
+
+The Password generator is a website hosted on https://kekehua.github.io/Password-Generator/ and can be used by going to the site, clicking the generate button, following the prompts, thus generating a password according to the users criteria.
+
+## Acceptance Criteria
+
+The Password generator will meet the following working criterion:
+
+GIVEN I need a new, secure password
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+WHEN asked for character types to include in the password
+THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+
+## Functionality
+
+Functions in script.js were editted to add functionality to the site
+Window Prompt was generated to ask users how many numbers the password should be
+    if/else statements created to reject values under 8 and over 128
+Criteria for Capital Letters, Special Letters, and Numbers are recorded through window prompts that record a boolean value
+    if statement created to reject answers that decline having any of these criteria
+Function created to create password once user answers the requisite window prompts
